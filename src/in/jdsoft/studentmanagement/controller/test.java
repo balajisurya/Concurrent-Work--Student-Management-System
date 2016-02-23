388d0c8439b7a59e26cd44901d3e082a46c25bbd
Nikil kumar <nikilkumar.s@jdsoft.in>
2016-02-23 11:01:50 +0530

    corrected completely

---
+++ src/in/jdsoft/studentmanagement/controller/test.java
@@ -0,0 +1,13 @@
+package in.jdsoft.studentmanagement.controller;
+
+public class test {
+	
+  public void add(){
+	  System.out.println("addition");
+  }
+  
+  public void sub(){
+	  System.out.println("subtraction");
+  }
+
+}
